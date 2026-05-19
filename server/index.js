4ae8a9caacf1fb8efea9f70009e170089fc83991
sickcurse <111002422+sickcurse@users.noreply.github.com>
2026-05-19 15:58:23 -0400

    fix port for deployment

--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/grocery-app')
+mongoose.connect('mongodb+srv://admin:lzlkMN3Rip5gmsVX@cluster0.8qhio3m.mongodb.net/grocery-app')
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
 app.use('/items', itemRoutes);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
