378e3e1641568c07ed55b9ac9d399b85931ec656
sickcurse <111002422+sickcurse@users.noreply.github.com>
2026-05-19 16:05:57 -0400

    remove dotenv for render deployment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
-
 const itemRoutes = require('./routes/items');
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
-
 app.use('/items', itemRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
